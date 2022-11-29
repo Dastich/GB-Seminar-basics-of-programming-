18da5a6fbdcf3dd326b5ecf18b686c864b494975
Maksim <roker777799@gmail.com>
2022-11-29 22:25:54 +0300

    end of 1st lesson

---
+++ task0/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите число: ");
+// string answer = Console.ReadLine();
+// int number = Convert.ToInt32(answer); //конвертация строки в число
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(number * number);

d5c32aac9e76443607873e96aa7e3510af7a1aaf
Maksim <roker777799@gmail.com>
2022-12-04 22:41:29 +0300

    complete 11th task

---
+++ task11/Program.cs
@@ -0,0 +1,6 @@
+﻿int number = new Random().Next(100,1000);
+Console.WriteLine($"Случайное число={number}");
+int first = number/100;
+int third = number%10;
+int result =first*10+third;
+Console.WriteLine(result);

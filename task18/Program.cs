d1e01fdb4b259708f3dd0c87097325253d8d0485
Maksim <roker777799@gmail.com>
2022-12-06 21:02:18 +0300

    Complete 18th task

---
+++ task18/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Введите номер координатной четверти:");
+int number=Convert.ToInt32(Console.ReadLine());
+
+while(number>4||number<0)
+{
+    Console.WriteLine("Введеный номер неккоректный, введите его повторно: ");
+     number=Convert.ToInt32(Console.ReadLine());
+}
+if(number==1)
+{
+    Console.WriteLine("Допустимые значения:x>0,y>0");
+}
+if(number==2)
+{
+    Console.WriteLine("Допустимые значения:x<0,y>0");
+}
+
+if(number==3)
+{
+    Console.WriteLine("Допустимые значения:x<0,y<0");
+}
+
+if(number==4)
+{
+    Console.WriteLine("Допустимые значения:x>0,y<0");
+}
+

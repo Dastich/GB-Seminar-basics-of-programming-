08cdeb58452d46854dd19bb360b94067ec10b853
Maksim <roker777799@gmail.com>
2022-12-06 20:36:05 +0300

    Complete 17th task

---
+++ task17/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите координаты точки по X: ");
+int x=Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координаты точки по Y: ");
+int y=Convert.ToInt32(Console.ReadLine());
+
+if(x>0&&y>0)
+{
+    Console.WriteLine("Точка находится в первой координатной четверти");
+}
+if(x<0&&y>0)
+{
+    Console.WriteLine("Точка находится во второй координатной четверти");
+}
+if(x<0&&y<0)
+{
+    Console.WriteLine("Точка находится в третьей координатной четверти");
+}
+if(x>0&&y<0)
+{
+    Console.WriteLine("Точка находится в четвертой координатной четверти");
+}

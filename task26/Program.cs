865e9e5b5436c66c4d4d3ab7f11cdc3b1572f86f
Maksim <roker777799@gmail.com>
2022-12-09 21:28:10 +0300

    complete 28th and 30th

---
+++ task26/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetCountOfNumber(int number)
+{
+    int count=0;
+    while(number !=0)
+    {
+        number/=10;
+        count+=1;
+    }
+    return count;
+}
+
+int number=ReadInt("Введите число: ");
+int res = GetCountOfNumber(number);
+
+Console.WriteLine($"В числе {number}-{res} цифр");
---
+++ task28/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetmultiplicationOfNumber(int number)
+{
+    int res=1;
+    for(int i=1;i<=number;i++)
+    {
+        res=res*i;
+    }
+    return res;
+}
+
+int number=ReadInt("Введите число: ");
+int result= GetmultiplicationOfNumber(number);
+Console.WriteLine($"Сумма произведения числе равна {result}");
